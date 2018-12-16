07869e031bcf5b48f611ec0914ef37175fc8e97f
Edouard Buschini <edouard.buschini@gmail.com>
2018-12-16 13:40:16 -0500

    Add build script

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+cd src/cryptocli
+go build .
+
+rm cryptocli-*
+
+GOPATH=$(pwd)/.go
+GOPATH=${GOPATH} go get -u -v ./...
+
+compile() {
+	GOOS=$1
+	GOARCH=$2
+	NAME="cryptocli-${GOOS}-${GOARCH}"
+
+	if [ "${GOOS}" = "windows" ]
+	then
+		NAME="${NAME}.exe"
+	fi
+
+	GOPATH=${GOPATH} GOOS=${GOOS} GOARCH=${GOARCH} go build -o "${NAME}" -tags netgo -ldflags "-w -extldflags \"-static\""
+	echo "Done compiling for ${GOOS} ${GOARCH}"
+	gzip "${NAME}"
+}
+
+compile darwin amd64
+compile linux amd64
+compile linux 386
+compile windows amd64
+compile windows 386
+compile openbsd amd64
+compile netbsd amd64
