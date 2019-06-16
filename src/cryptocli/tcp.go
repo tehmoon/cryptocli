1e9a236de451be68fd1a1bc7860610442aba233d
Edouard Buschini <edouard.buschini@gmail.com>
2019-06-16 07:37:43 -0400

    Add tls options for tcp module

--- src/cryptocli/tcp.go
+++ src/cryptocli/tcp.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"net"
 	"github.com/tehmoon/errors"
 	"github.com/spf13/pflag"
@@ -17,11 +18,15 @@ type TCP struct {
 	out chan *Message
 	sync *sync.WaitGroup
 	addr string
-	conn *net.TCPConn
+	conn net.Conn
+	tls string
+	insecure bool
 }
 
 func (m *TCP) SetFlagSet(fs *pflag.FlagSet) {
 	fs.StringVar(&m.addr, "addr", "", "Tcp address to connect to")
+	fs.StringVar(&m.tls, "tls", "", "Use TLS with servername in client hello")
+	fs.BoolVar(&m.insecure, "insecure", false, "Don't verify certificate chain when \"--tls\" is set")
 }
 
 func (m *TCP) In(in chan *Message) (chan *Message) {
@@ -47,6 +52,15 @@ func (m *TCP) Init(global *GlobalFlags) (error) {
 		return errors.Wrap(err, "Fail to dial tcp")
 	}
 
+	if m.tls != "" || m.insecure {
+		config := &tls.Config{
+			InsecureSkipVerify: m.insecure,
+			ServerName: m.tls,
+		}
+
+		m.conn = tls.Client(m.conn, config)
+	}
+
 	log.Printf("Tcp connection established to %s\n", addr.String())
 
 	return nil
@@ -67,28 +81,28 @@ func (m TCP) Wait() {
 	for range m.in {}
 }
 
-func tcpStartIn(conn *net.TCPConn, in chan *Message, wg *sync.WaitGroup) {
+func tcpStartIn(conn net.Conn, in chan *Message, wg *sync.WaitGroup) {
+	defer conn.Close()
+	defer wg.Done()
+
 	for message := range in {
 		_, err := conn.Write(message.Payload)
 		if err != nil {
 			log.Println(errors.Wrap(err, "Error writing to tcp connection in tcp"))
-			break
+			return
 		}
 	}
-
-	conn.CloseRead()
-	wg.Done()
 }
 
-func tcpStartOut(conn *net.TCPConn, out chan *Message, wg *sync.WaitGroup) {
+func tcpStartOut(conn net.Conn, out chan *Message, wg *sync.WaitGroup) {
+	defer close(out)
+	defer wg.Done()
+
 	err := ReadBytesSendMessages(conn, out)
 	if err != nil {
 		log.Println(errors.Wrap(err, "Error reading tcp connection in tcp"))
+		return
 	}
-
-	conn.CloseWrite()
-	close(out)
-	wg.Done()
 }
 
 func NewTCP() (Module) {
