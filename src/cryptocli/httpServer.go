0af45c5d2f7d3a286e85027f37506c03a09f723d
Edouard Buschini <edouard.buschini@gmail.com>
2019-06-16 07:43:20 -0400

    Same changes for http-server as for tcp-server

--- src/cryptocli/httpServer.go
+++ src/cryptocli/httpServer.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"time"
 	"io"
 	"net/http"
 	"github.com/tehmoon/errors"
@@ -15,24 +16,27 @@ func init() {
 }
 
 type HTTPServerHandleOptions struct {
-	Sync *sync.WaitGroup
-	Mutex *sync.Mutex
+	Waitc chan struct{}
+	Relayc chan *Message
 	Init bool
+	Sync *sync.WaitGroup
+
+	sync.Mutex
 }
 
 type HTTPServer struct {
 	in chan *Message
 	out chan *Message
-	sync *sync.WaitGroup
 	req *http.Request
 	addr string
 	ln net.Listener
-	mutex *sync.Mutex
-	init bool
+	connectTimeout time.Duration
+	sync *sync.WaitGroup
 }
 
 func (m *HTTPServer) SetFlagSet(fs *pflag.FlagSet) {
 	fs.StringVar(&m.addr, "addr", "", "Listen on an address")
+	fs.DurationVar(&m.connectTimeout, "connect-timeout", 30 * time.Second, "Max amount of time to wait for a potential connection when pipeline is closing")
 }
 
 func (m *HTTPServer) In(in chan *Message) (chan *Message) {
@@ -79,34 +83,43 @@ func httpServerHandleOut(out chan *Message, body io.ReadCloser, wait *sync.WaitG
 
 func httpServerHandle(in, out chan *Message, options *HTTPServerHandleOptions) (func(w http.ResponseWriter, r *http.Request)) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		options.Mutex.Lock()
-		defer options.Mutex.Unlock()
+		options.Lock()
 
-		if ! options.Init {
-			defer options.Sync.Done()
-			options.Init = true
-
-			// done synchronises the write part of the 2 go routines
-			done := &sync.WaitGroup{}
-			done.Add(1)
-
-			// wait tells the write part to start writing when read is done
-			// this is because the http specs says we should read the body
-			// before writing
-			wait := &sync.WaitGroup{}
-			wait.Add(1)
-
-			go httpServerHandleIn(in, w, wait, done)
-			go httpServerHandleOut(out, r.Body, wait)
-
-			done.Wait()
-
+		if options.Init {
+			w.WriteHeader(500)
+			options.Unlock()
 			return
 		}
+
+		defer options.Sync.Done()
+
+		options.Init = true
+		options.Unlock()
+
+		options.Waitc <- struct{}{}
+
+		// done synchronises the write part of the 2 go routines
+		done := &sync.WaitGroup{}
+		done.Add(1)
+
+		// wait tells the write part to start writing when read is done
+		// this is because the http specs says we should read the body
+		// before writing
+		wait := &sync.WaitGroup{}
+		wait.Add(1)
+
+		go httpServerHandleIn(in, w, wait, done)
+		go httpServerHandleOut(out, r.Body, wait)
+
+		done.Wait()
 	}
 }
 
 func (m *HTTPServer) Init(global *GlobalFlags) (error) {
+	if m.connectTimeout < 1 {
+		return errors.Errorf("Flag %q cannot be negative or zero", "--connect-timeout")
+	}
+
 	addr, err := net.ResolveTCPAddr("tcp", m.addr)
 	if err != nil {
 		return errors.Wrap(err, "Unable to resolve tcp address")
@@ -122,21 +135,67 @@ func (m *HTTPServer) Init(global *GlobalFlags) (error) {
 	return nil
 }
 
-func (m HTTPServer) Start() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", httpServerHandle(m.in, m.out, &HTTPServerHandleOptions{
+func (m *HTTPServer) Start() {
+	waitc := make(chan struct{})
+	relayc := make(chan *Message, 1)
+
+	m.sync.Add(1)
+
+	options := &HTTPServerHandleOptions{
+		Init: false,
+		Waitc: waitc,
 		Sync: m.sync,
-		Mutex: m.mutex,
-		Init: m.init,
-	}))
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", httpServerHandle(relayc, m.out, options))
 
 	server := &http.Server{
 		Handler: mux,
 	}
 
-	m.sync.Add(1)
+	startRelay := sync.WaitGroup{}
+	startRelay.Add(1)
 
 	go server.Serve(m.ln)
+	go func() {
+		select {
+			case <- waitc:
+				startRelay.Done()
+			case message, closed := <- m.in:
+				select {
+					case <- time.NewTicker(m.connectTimeout).C:
+						log.Println("Connect timeout reached, let's hope somebody's connected")
+					case <- waitc:
+				}
+
+				options.Lock()
+				if closed && ! options.Init {
+					options.Init = true
+					options.Unlock()
+
+					log.Println("Pipeline is shutting down and nobody connected")
+					close(m.out)
+					m.sync.Done()
+					return
+				}
+
+				options.Unlock()
+
+				relayc <- message
+				startRelay.Done()
+		}
+	}()
+
+	go func() {
+		startRelay.Wait()
+
+		for message := range m.in {
+			relayc <- message
+		}
+
+		close(relayc)
+	}()
 }
 
 func (m HTTPServer) Wait() {
@@ -148,7 +207,5 @@ func (m HTTPServer) Wait() {
 func NewHTTPServer() (Module) {
 	return &HTTPServer{
 		sync: &sync.WaitGroup{},
-		mutex: &sync.Mutex{},
-		init: false,
 	}
 }
