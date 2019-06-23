b961d377f8c3b7040a4ad268a7e86c45645f369d
Edouard Buschini <ebuschini@preveil.com>
2019-06-23 15:17:23 -0400

    Update generate_doc using jq

--- generate_doc.sh
+++ generate_doc.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
+set -e
 
-{ printf '### Cryptocli\n\n```\n'; ./src/cryptocli/cryptocli -h 2>&1; printf '```\n\n### Modules\n\n'; mods=$(./src/cryptocli/cryptocli -h 2>&1 | grep -E '^\t([a-z0-9\-]+):' | cut -d ':' -f 1); for mod in $(echo $mods); do printf '```\n' ; ./src/cryptocli/cryptocli --  "${mod}" -h 2>&1 ; printf '```\n';done ; }
+printf '### Cryptocli\n\n```\n'
+usage=$(./src/cryptocli/cryptocli -h 2>&1 || true)
+
+echo "${usage}"
+printf '```\n\n### Modules\n\n'
+mods=$(echo "${usage}" | jq -nrR 'inputs | select(test("^\t[a-z0-9-]+:")) | split(":")[0][1:]')
+
+for mod in $mods; do
+	printf '```\n'
+	./src/cryptocli/cryptocli -- "${mod}" -h 2>&1 || true
+	printf '```\n'
+done
